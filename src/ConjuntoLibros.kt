aaa016916d928e48c5cabb580d02e9ad1ca40538
gromber05 <gromber2911@g.educaand.es>
2025-01-30 00:24:08 +0100

    Avanzada la clase ConjuntoLibros

--- src/ConjuntoLibros.kt
+++ src/ConjuntoLibros.kt
@@ -1,6 +1,45 @@
 
-class ConjuntoLibros() {
+class ConjuntoLibros(lista: Array<Libro?>) {
 
     var lista: Array<Libro?> = Array(3) { null }
+
+    fun anadirLibro(libro: Libro) {
+        lista.forEachIndexed { index, elemento ->
+            if (elemento == null ) {
+                lista[index] = libro
+                return@forEachIndexed
+            }
+            else {
+                println("No se ha podido agregar ningún libro")
+            }
+        }
+    }
+
+    fun eliminarLibro(campo: String) {
+        lista.forEachIndexed { index, elemento ->
+            if (elemento?.autor == campo || elemento?.titulo == campo ) {
+                lista[index] = null
+                return@forEachIndexed
+            }
+            else {
+                println("No se ha podido encontrar/eliminar el libro")
+            }
+        }
+    }
+
+
+    fun mostrarLibro() {
+        var minimo = lista.filterNotNull().minOfOrNull { it.calificacion }
+        var maximo = lista.filterNotNull().maxOfOrNull { it.calificacion }
+
+        println("")
+
+    }
+
+    fun mostrarTodo() {
+        lista.forEach { libro ->
+            println("Titulo: ${libro?.titulo} | Autor: ${libro?.autor} | Número de páginas: ${libro?.numPaginas} | Calificación: ${libro?.numPaginas}")
+        }
+    }
 
 }
